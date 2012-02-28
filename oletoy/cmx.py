939363e9caa44c5c2b20755d6b3e709af56059c7
Valek Filippov <frob@frob-laptop>
2012-02-27 22:35:07 -0500

    CMX: more 'cont'.

--- oletoy/cmx.py
+++ oletoy/cmx.py
@@ -35,6 +35,55 @@ def cont (hd,size,data):
 	# coord size: "2" - 16 bits, "4" - 32 bits
 	add_iter (hd, "Coord Size", data[off:off+2],off,2,"<H")
 	off += 2
+	# version major: "1" - 16 bits, "2" - 32 bits
+	add_iter (hd, "Version maj", data[off:off+4],off,4,"<I")
+	off += 4
+	# version minor: "0"
+	add_iter (hd, "Version min", data[off:off+4],off,4,"<I")
+	off += 4
+	# coord units: 0x23 - mm, 0x40 - inches
+	add_iter (hd, "Coord Units", struct.unpack("<H",data[off:off+2])[0],off,2,"<H")
+	off += 2
+	# precision factor
+	add_iter (hd, "Precision", struct.unpack("<d",data[off:off+8])[0],off,8,"<d")
+	off += 8
+	# not used "Option", "FrgnKey", "Capability"
+	add_iter (hd, "Not used", "",off,12,"txt")
+	off += 12
+	# index section offset
+	add_iter (hd, "Idx Section Offset", struct.unpack("<I",data[off:off+4])[0],off,4,"<I")
+	off += 4
+	# info section offset
+	add_iter (hd, "Info Section Offset", struct.unpack("<I",data[off:off+4])[0],off,4,"<I")
+	off += 4
+	# Thumbnail section offset
+	add_iter (hd, "Thumbnail Section Offset", struct.unpack("<I",data[off:off+4])[0],off,4,"<I")
+	off += 4
+	# min X coord in the file
+	add_iter (hd, "Min X coord", struct.unpack("<i",data[off:off+4])[0],off,4,"<i")
+	off += 4
+	# max Y coord in the file
+	add_iter (hd, "Max Y coord", struct.unpack("<i",data[off:off+4])[0],off,4,"<i")
+	off += 4
+	# max X coord in the file
+	add_iter (hd, "Max X coord", struct.unpack("<i",data[off:off+4])[0],off,4,"<i")
+	off += 4
+	# min Y coord in the file
+	add_iter (hd, "Min Y coord", struct.unpack("<i",data[off:off+4])[0],off,4,"<i")
+	off += 4
+	# num of instructions in pages
+	add_iter (hd, "# of instructions", struct.unpack("<I",data[off:off+4])[0],off,4,"<I")
+	off += 4
+	# reserved
+	add_iter (hd, "Reserved", "",off,64,"txt")
+	off += 64
+
+
+	
+
+	
+	
+	
 
 cmx_ids = {"cont":cont}
 
