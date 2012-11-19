11482d70dcdc8a5a6133e5374dfbdbcd5fdf4695
Fridrich Štrba <fridrich.strba@bluewin.ch>
2012-11-19 11:14:09 +0100

    the shape offsets seems to be for me these ones

--- oletoy/vsdchunks5.py
+++ oletoy/vsdchunks5.py
@@ -110,11 +110,11 @@ def List (hd, size, value):
 
 def Shape (hd, size, value):
 	add_iter (hd, "Parent", "%2x"%struct.unpack("<H",value[0xe:0x10])[0],0xe,2,"<H")
-	add_iter (hd, "Master", "%2x"%struct.unpack("<H",value[0x10:0x12])[0],0x10,2,"<H")
-	add_iter (hd, "MasterShape", "%2x"%struct.unpack("<H",value[0x12:0x14])[0],0x12,2,"<H")
-	add_iter (hd, "FillStyle", "%2x"%struct.unpack("<H",value[0x16:0x18])[0],0x16,2,"<H")
-	add_iter (hd, "LineStyle", "%2x"%struct.unpack("<H",value[0x18:0x1a])[0],0x18,2,"<H")
-	add_iter (hd, "TextStyle", "%2x"%struct.unpack("<H",value[0x1a:0x1c])[0],0x1a,2,"<H")
+	add_iter (hd, "Master", "%2x"%struct.unpack("<H",value[0x12:0x14])[0],0x12,2,"<H")
+	add_iter (hd, "MasterShape", "%2x"%struct.unpack("<H",value[0x14:0x16])[0],0x14,2,"<H")
+	add_iter (hd, "FillStyle", "%2x"%struct.unpack("<H",value[0x18:0x1a])[0],0x18,2,"<H")
+	add_iter (hd, "LineStyle", "%2x"%struct.unpack("<H",value[0x1a:0x1c])[0],0x1a,2,"<H")
+	add_iter (hd, "TextStyle", "%2x"%struct.unpack("<H",value[0x1c:0x1e])[0],0x1c,2,"<H")
 
 def NameID (hd, size, value):
 	numofrec = struct.unpack("<H",value[12:12+2])[0]
