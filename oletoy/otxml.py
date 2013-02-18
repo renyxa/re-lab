6598bfb0f64707df01a538b9916b2e644548b8c5
Valek Filippov <frob@df.ru>
2013-02-18 00:48:15 -0500

    XML: add 'parser'.

---
+++ oletoy/otxml.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2007,2010,2011	Valek Filippov (frob@df.ru)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 or later of the GNU General Public
+# License as published by the Free Software Foundation.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
+#
+
+import sys,struct,gtk,gobject
+from utils import *
+
+def open (buf,page,parent):
+	print "XML"
+	t = buf.split("<")
+	piter = parent
+	citer = []
+
+	for i in range(len(t)-1):
+		foo = t[i+1].split()
+		if len(foo) > 0:
+			bar = foo[0]
+		else:
+			bar = foo[:-1]
+		
+		if bar[0] == "?":
+			id = bar[1:]
+		else:
+			id = bar
+		
+		if id[-1] == ">":
+			id = id[:-1]
+		else:
+			id = id.replace(">",": ")
+		
+		data = "<"+t[i+1].replace("\x0d\x0a","")  # all from tag start till next tag
+
+		if bar[0] != "/":
+			if t[i+1][-2:-1] != "/":
+				d = data.split(">")
+				niter = add_pgiter (page,id,"xml","",d[0]+">",piter)
+				if len(d[1]) > 0:
+					add_pgiter (page,id,"xml","sub",d[1],niter)
+				citer.append(piter)
+				piter = niter
+			else:
+				niter = add_pgiter (page,id,"xml","",data,piter)
+		else:
+			piter = citer.pop()
+
+	return "xml"
