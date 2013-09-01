37aa20e3f5ce1a39d06b0558fe983567f4c53f5b
David Tardon <dtardon@redhat.com>
2013-09-01 14:29:55 +0200

    zbr: add file iterator back
    I want to see the size of the complete file...

--- oletoy/zbr.py
+++ oletoy/zbr.py
@@ -29,8 +29,10 @@ class Parser(object):
 		self.data = data
 		self.page = page
 		self.parent = parent
+		self.fileiter = None
 
 	def parse(self):
+		self.fileiter = add_pgiter(self.page, 'ZBR', 'zbr', 0, self.data, self.parent)
 		off = self.parse_header()
 		off = self.parse_preview(off)
 		off = self.parse_configuration(off)
@@ -38,11 +40,11 @@ class Parser(object):
 		self.parse_objects(off)
 
 	def parse_header(self):
-		add_pgiter(self.page, 'Header', 'zbr', 'header', self.data[0:104], self.parent)
+		add_pgiter(self.page, 'Header', 'zbr', 'header', self.data[0:104], self.fileiter)
 		return 104
 
 	def parse_preview(self, off):
-		previter = add_pgiter(self.page, 'Preview bitmap', 'zbr', 0, self.data[off:off + 5264], self.parent)
+		previter = add_pgiter(self.page, 'Preview bitmap', 'zbr', 0, self.data[off:off + 5264], self.fileiter)
 		add_pgiter(self.page, 'DIB palette', 'zbr', 'palette', self.data[off:off + 64], previter)
 		off += 64
 		dibiter = add_pgiter(self.page, 'DIB data', 'zbr', 0, self.data[off:off + 5200], previter)
@@ -52,7 +54,7 @@ class Parser(object):
 		(length, off) = rdata(self.data, off, '<I')
 		length = int(length)
 		data = self.data[off - 4:off + length]
-		confiter = add_pgiter(self.page, 'Configuration', 'zbr', 'configuration', data, self.parent)
+		confiter = add_pgiter(self.page, 'Configuration', 'zbr', 'configuration', data, self.fileiter)
 		add_pgiter(self.page, 'Local configuration', 'zbr', 'config_local', data[4:], confiter)
 		return off + length
 
@@ -60,12 +62,12 @@ class Parser(object):
 		(length, off) = rdata(self.data, off, '<I')
 		length = int(length)
 		data = self.data[off - 4:off + length]
-		palette_iter = add_pgiter(self.page, 'Color Palette', 'zbr', 'color_palette', data, self.parent)
+		palette_iter = add_pgiter(self.page, 'Color Palette', 'zbr', 'color_palette', data, self.fileiter)
 		add_pgiter(self.page, 'Palette', 'zbr', 'palette', data[4:], palette_iter)
 		return off + length
 
 	def parse_objects(self, off):
-		objsiter = add_pgiter(self.page, 'Objects', 'zbr', 0, self.data[off:], self.parent)
+		objsiter = add_pgiter(self.page, 'Objects', 'zbr', 0, self.data[off:], self.fileiter)
 		return len(self.data)
 
 def _add_length(hd, size, data):
