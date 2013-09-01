aeb31d0e4d32709a3c562f5e9feab7a9078ee3e5
David Tardon <dtardon@redhat.com>
2013-09-01 19:09:08 +0200

    do not fail on detection of zmf files v.3
    ... which can contain file named Contents, but it is empty in all the
    files I have.

--- oletoy/ole.py
+++ oletoy/ole.py
@@ -188,7 +188,7 @@ def gsf_get_children(page,infile,parent,ftype,dirflag=0):
 				page.model.set_value(iter1,1,("quill",dirflag))
 				quill.parse (page,data,iter1)
 		if infname == "Contents":
-			if data[:2] == "\xe8\xac": # take signature into account
+			if data and data[:2] == "\xe8\xac": # take signature into account
 				ftype = "pub"
 				page.model.set_value(iter1,1,("pub",dirflag))
 				pub.parse (page,data,iter1)
