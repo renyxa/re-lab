7d6dd7cea938146e0d492d0ecaf77bf54f3beaf1
David Tardon <dtardon@redhat.com>
2013-09-01 13:59:28 +0200

    zmf: guess structure of content

--- oletoy/zmf.py
+++ oletoy/zmf.py
@@ -30,13 +30,37 @@ class ZMF5Parser(object):
 
 	def parse(self):
 		content = self.parse_header()
-		add_pgiter(self.page, 'Content', 'zmf', 0, self.data[content:], self.parent)
+		self.parse_content(content)
 
 	def parse_header(self):
 		offset = int(read(self.data, 0x20, '<I'))
 		data = self.data[0:offset]
 		add_pgiter(self.page, 'Header', 'zmf', 'zmf5_header', data, self.parent)
 		return offset
+
+	def parse_content(self, begin):
+		data = self.data[begin:]
+		content_iter = add_pgiter(self.page, 'Content', 'zmf', 0, data, self.parent)
+		self._parse_group(data, content_iter)
+
+	def parse_object(self, data, parent):
+		objiter = add_pgiter(self.page, 'Object', 'zmf', 'zmf5_object', data, parent)
+		add_pgiter(self.page, 'Header', 'zmf', 'zmf5_object_header', data[0:32], objiter)
+		off = 4
+		# TODO: this is probably set of flags
+		(typ, off) = rdata(data, off, '<I')
+		if typ == 0xc or typ == 0xd:
+			self._parse_group(data[32:], objiter)
+		else:
+			add_pgiter(self.page, 'Data', 'zmf', 0, data[32:], objiter)
+
+	def _parse_group(self, data, parent):
+		off = 0
+		while off + 4 <= len(data):
+			length = int(read(data, off, '<I'))
+			if off + length <= len(data):
+				self.parse_object(data[off:off + length], parent)
+			off += length
 
 def parse_header(page, data, parent):
 	pass
@@ -67,8 +91,15 @@ def add_zmf5_header(hd, size, data):
 	(size, off) = rdata(data, off, '<I')
 	add_iter(hd, 'File size', size, off - 4, 4, '<I')
 
+def add_zmf5_object_header(hd, size, data):
+	(size, off) = rdata(data, 0, '<I')
+	add_iter(hd, 'Size', size, off - 4, 4, '<I')
+	(typ, off) = rdata(data, off, '<I')
+	add_iter(hd, 'Type', typ, off - 4, 4, '<I')
+
 zmf_ids = {
 	'zmf5_header': add_zmf5_header,
+	'zmf5_object_header': add_zmf5_object_header,
 }
 
 def zmf3_open(page, data, parent, fname):
