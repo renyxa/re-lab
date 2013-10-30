22c037b9f394b289f2bc9b0242c0e2de475fd78a
Valek Filippov <frob@df.ru>
2013-10-29 23:01:43 -0400

    FBX: parse props (if <1000, need to migrate to idle loop).

--- oletoy/fbx.py
+++ oletoy/fbx.py
@@ -17,7 +17,49 @@ import struct
 from utils import *
 
 
-
+def parse_props(page,buf,off,pnum,parent):
+	for i in range(pnum):
+		ptype = buf[off]
+		if ptype == "Y":
+			plen = 2
+			res = struct.unpack("<h",buf[off+1:off+3])[0]
+		elif ptype == "C":
+			plen = 1
+			res = ord(buf[off+1])
+		elif ptype == "I":
+			plen = 4
+			res = struct.unpack("<i",buf[off+1:off+5])[0]
+		elif ptype == "F":
+			plen = 4
+			res = struct.unpack("<f",buf[off+1:off+5])[0]
+		elif ptype == "D":
+			plen = 8
+			res = struct.unpack("<d",buf[off+1:off+9])[0]
+		elif ptype == "L":
+			plen = 8
+			res = struct.unpack("<q",buf[off+1:off+9])[0]
+		elif ptype == "S" or ptype == "R":
+			slen = struct.unpack("<I",buf[off+1:off+5])[0]
+			plen = slen+4
+			res = buf[off+5:off+5+slen]
+		elif ptype in ("f","d","l","i","b"):
+			alen = struct.unpack("<I",buf[off+1:off+5])[0]
+			enc = struct.unpack("<I",buf[off+5:off+9])[0]
+			clen = struct.unpack("<q",buf[off+9:off+13])[0]
+			if enc == 0:
+				if ptype == "b":
+					plen = 12+alen
+				elif ptype == "i" or ptype == "f":
+					plen = 12+4*alen
+				elif ptype == "d" or ptype == "l":
+					plen = 12+8*alen
+				res = ""
+			else:
+				plen = 12+clen
+		else:
+			print "Unknown prop type:",ptype
+		add_pgiter(page,"%s: %s"%(ptype,res),"fbx","prop",buf[off:off+1+plen],parent)
+		off += 1+plen
 
 def parse_block(page,buf,off,noff,parent):
 	iters = [parent]
@@ -33,6 +75,9 @@ def parse_block(page,buf,off,noff,parent):
 			if propnum == 0:
 				iters.append(piter)
 				parent = piter
+			elif propnum < 1000: #FIXME! Takes to long to parse everything
+				parse_props(page,buf,off+13+namelen,propnum,piter)
+				
 		else:
 			try:
 				iters.pop()
@@ -59,4 +104,4 @@ def open (buf,page,parent,off=0):
 		else:
 			break
 	add_pgiter(page,"Tail","fbx","tail",buf[off:],parent)
-	
+
