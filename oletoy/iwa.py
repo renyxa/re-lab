f963f1207313eb0f4235098ba2e95b952894e3a3
David Tardon <dtardon@redhat.com>
2014-03-02 14:52:12 +0100

    iwa: show the file structure

--- oletoy/iwa.py
+++ oletoy/iwa.py
@@ -18,6 +18,9 @@ import struct
 
 from utils import add_iter, add_pgiter, rdata
 
+def read(data, offset, fmt):
+	return rdata(data, offset, fmt)[0]
+
 def read_var(data, offset):
 	"""Read a variable length number."""
 
@@ -126,6 +129,56 @@ def uncompress(data):
 
 	return result
 
+class IWAParser(object):
+
+    def __init__(self, data, page, parent):
+	self.data = data
+	self.page = page
+	self.parent = parent
+
+    def parse(self):
+	off = 0
+	i = 0
+	while off < len(self.data):
+	    start = off
+	    (off, length) = self._parse_header(off)
+	    header_end = off
+	    # TODO: maybe the reflist is allowed even if there is no content?
+	    if length > 0:
+		c = int(read(self.data, off, '<B'))
+		if c & 0xf0 == 0x20:
+		    off = self._parse_reflist(off)
+
+	    objiter = add_pgiter(self.page, 'Object %d' % i, 'iwa', 0, self.data[start:off + length], self.parent)
+	    add_pgiter(self.page, 'Header', 'iwa', 'iwa_object_header', self.data[start:header_end], objiter)
+	    if length > 0:
+		if c & 0xf0 == 0x20:
+		    # TODO: what does this mean, exactly? Should it perhaps be a part of header?
+		    add_pgiter(self.page, 'List of references', 'iwa', 'iwa_reflist', self.data[header_end:off], objiter)
+		off = self._parse_content(off, length, objiter)
+
+	    i += 1
+
+	# assert off == len(self.data)
+
+    def _parse_header(self, offset):
+	(oid, off) = read_var(self.data, offset + 2)
+	off += 3
+	(xid, off) = read_var(self.data, off)
+	off += 6
+	(length, off) = read_var(self.data, off)
+	return (off, length)
+
+    def _parse_reflist(self, offset):
+	(c, off) = rdata(self.data, offset, '<B')
+	assert int(c) & 0xf0 == 0x20
+	(length, off) = read_var(self.data, off)
+	return off + int(length)
+
+    def _parse_content(self, offset, length, parent):
+	add_pgiter(self.page, 'Content', 'iwa', 0, self.data[offset:offset + length], parent)
+	return offset + length
+
 def add_iwa_compressed_block(hd, size, data):
 	(length, off) = rdata(data, 1, '<H')
 	add_iter(hd, 'Compressed length', length, off - 2, 2, '<H')
@@ -134,8 +187,46 @@ def add_iwa_compressed_block(hd, size, data):
 	(ulength, off) = read_var(data, off)
 	add_iter(hd, 'Uncompressed length', ulength, var_off, off - var_off, '%ds' % (off - var_off))
 
+def add_iwa_object_header(hd, size, data):
+    (flags, off) = rdata(data, 0, '<B')
+    # Flags? or a type of the object?
+    add_iter(hd, 'Flags???', '0x%x' % flags, off - 1, 1, '<B')
+    off += 1
+    orig = off
+    (oid, off) = read_var(data, off)
+    add_iter(hd, 'Object ID', oid, orig, off - orig, '%ds' % (off - orig))
+    off += 1
+    (more_flags, off) = rdata(data, off, '<B')
+    add_iter(hd, 'More flags???', '0x%x' % more_flags, off - 1, 1, '<B')
+    off += 1
+    orig = off
+    (ref, off) = read_var(data, off)
+    add_iter(hd, 'ID / Reference???', ref, orig, off - orig, '%ds' % (off - orig))
+    off += 1
+    off += 5
+    orig = off
+    (length, off) = read_var(data, off)
+    add_iter(hd, 'Length of content', length, orig, off - orig, '%ds' % (off - orig))
+
+def add_iwa_reflist(hd, size, data):
+    (flags, off) = rdata(data, 0, '<B')
+    add_iter(hd, 'Flags???', '0x%x' % flags, off - 1, 1, '<B')
+    orig = off
+    (length, off) = read_var(data, off)
+    add_iter(hd, 'Length', length, orig, off - orig, '%ds' % (off - orig))
+
+    i = 0
+    end = off + int(length)
+    while off < end:
+	orig = off
+	(ref, off) = read_var(data, off)
+	add_iter(hd, 'Reference %d' % i, ref, orig, off - orig, '%ds' % (off - orig))
+	i += 1
+
 iwa_ids = {
 	'iwa_compressed_block': add_iwa_compressed_block,
+	'iwa_object_header': add_iwa_object_header,
+	'iwa_reflist': add_iwa_reflist,
 }
 
 def open(data, page, parent):
@@ -157,6 +248,9 @@ def open(data, page, parent):
 		n += 1
 		off += length
 
-	add_pgiter(page, 'Uncompressed', 'iwa', 0, str(uncompressed_data), parent)
+	uncompressed_data = str(uncompressed_data)
+	dataiter = add_pgiter(page, 'Uncompressed', 'iwa', 0, uncompressed_data, parent)
+	parser = IWAParser(uncompressed_data, page, dataiter)
+	parser.parse()
 
 # vim: set ft=python sts=4 sw=4 noet:
