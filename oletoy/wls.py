82b503a2fd461684302bedf87b6d06c302c42eb9
David Tardon <dtardon@redhat.com>
2015-09-12 16:24:22 +0200

    wls: deobfuscate
    Currently everything but record size is deobfuscated, which is likely
    incorrect.

--- oletoy/wls.py
+++ oletoy/wls.py
@@ -18,6 +18,52 @@ import struct
 
 from utils import add_iter, add_pgiter, rdata
 
+obfuscation_map = {}
+
+def deobfuscate(data, orig_pos):
+	def get_obfuscation_map(pos):
+		def obfuscate_byte(byte, pos):
+			def usub(a, b):
+				if a >= b:
+					return a - b
+				else:
+					return 0x100 - b + a
+			def uadd(a, b):
+				return (a + b) & 0xff
+			val = byte
+			base = (val & 0x60) << 1
+			val = usub(base, val)
+			val = usub(0xb9, val)
+			mask = 0x1
+			for i in range(0, 5):
+				v = byte & mask
+				p = pos & mask
+				if p != 0:
+					if v == 0:
+						val = uadd(val, p)
+					else:
+						val = usub(val, p)
+				mask = mask << 1
+			mask = 0x60
+			p = pos & mask
+			if p != 0:
+				val = uadd(val, p)
+				val = uadd(val, base)
+			return val
+		if not obfuscation_map.has_key(pos):
+			d = {}
+			for b in range(0, 0x100):
+				d[chr(obfuscate_byte(b, pos))] = chr(b)
+			assert(len(d) == 0x100)
+			obfuscation_map[pos] = d
+		return obfuscation_map[pos]
+	new_data = []
+	pos = orig_pos
+	for b in data:
+		new_data.append(get_obfuscation_map(pos)[b])
+		pos += 1
+	return new_data
+
 class wls_parser(object):
 
 	def __init__(self, page, data, parent):
@@ -35,7 +81,12 @@ class wls_parser(object):
 				size = -size
 			if off + size > len(data):
 				break
-			add_pgiter(self.page, 'Record %d' % n, 'wls', '', data[off - 2:off + size], self.parent)
+			recdata = data[off - 2:off + size]
+			reciter = add_pgiter(self.page, 'Record %d' % n, 'wls', '', recdata, self.parent)
+			global recnum
+			recnum = n
+			content = deobfuscate(recdata[2:], 2)
+			add_pgiter(self.page, 'Deobfuscated content', 'wls', '', content, reciter)
 			off += size
 			n += 1
 		if off < len(data):
@@ -47,5 +98,49 @@ wls_ids = {
 def parse(page, data, parent):
 	parser = wls_parser(page, data, parent)
 	parser.parse()
+
+if __name__ == '__main__':
+	def test_deobfuscate(byte, pos, expected):
+		got = deobfuscate([chr(byte)], pos)
+		if got != [expected]:
+			print("expected '%s' (%x), got %x" % (expected, ord(expected), ord(got[0])))
+		assert(got == [expected])
+	test_deobfuscate(0x5f, 6, '`')
+	test_deobfuscate(0x60, 6, 'a')
+	test_deobfuscate(0x5d, 6, 'b')
+	test_deobfuscate(0x67, 6, 'h')
+	test_deobfuscate(0x68, 6, 'i')
+	test_deobfuscate(0x7f, 6, '@')
+	test_deobfuscate(0x82, 6, 'O')
+
+	test_deobfuscate(0x5f, 7, 'a')
+	test_deobfuscate(0x68, 7, 'h')
+	test_deobfuscate(0x67, 7, 'i')
+	test_deobfuscate(0x7f, 7, 'A')
+
+	test_deobfuscate(0xa9, 0x10, ' ')
+	test_deobfuscate(0xb8, 0x10, '/')
+	test_deobfuscate(0x99, 0x10, '0')
+	test_deobfuscate(0xa8, 0x10, '?')
+	test_deobfuscate(0x89, 0x10, '@')
+	test_deobfuscate(0x98, 0x10, 'O')
+	test_deobfuscate(0x79, 0x10, 'P')
+	test_deobfuscate(0x88, 0x10, '_')
+	test_deobfuscate(0x69, 0x10, '`')
+	test_deobfuscate(0x78, 0x10, 'o')
+	test_deobfuscate(0x59, 0x10, 'p')
+	test_deobfuscate(0x66, 0x10, '}')
+
+	test_deobfuscate(0x69, 0x11, 'a')
+	test_deobfuscate(0x6c, 0x11, 'b')
+	test_deobfuscate(0x72, 0x11, 'h')
+
+	test_deobfuscate(0x97, 0x1e, '@')
+
+	test_deobfuscate(0x39, 0x20, '`')
+	test_deobfuscate(0x3a, 0x20, 'a')
+	test_deobfuscate(0x3b, 0x20, 'b')
+	test_deobfuscate(0x19, 0x20, '@')
+	test_deobfuscate(0x1a, 0x20, 'A')
 
 # vim: set ft=python ts=4 sw=4 noet:
