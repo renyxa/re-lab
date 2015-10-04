3b749516fd32abc7ca01beceacfc1cd103d1c287
David Tardon <dtardon@redhat.com>
2015-10-04 20:02:08 +0200

    c602: parse spreadsheet file structure

--- oletoy/c602.py
+++ oletoy/c602.py
@@ -16,6 +16,11 @@
 
 from utils import add_iter, add_pgiter, rdata
 
+tc6_records = {
+	0x1: ('Start sheet?', None),
+	0xff: ('End sheet?', None),
+}
+
 class tc6_parser:
 	def __init__(self, page, data, parent):
 		self.data = data
@@ -23,7 +28,21 @@ class tc6_parser:
 		self.parent = parent
 
 	def parse(self):
-		pass
+		assert len(self.data) > 0x40
+		add_pgiter(self.page, 'Header', 'c602', 'tc6_header', self.data[0:0x40], self.parent)
+		off = 0x40
+		while off + 3 <= len(self.data):
+			(rec, off) = rdata(self.data, off, '<B')
+			(length, off) = rdata(self.data, off, '<H')
+			if tc6_records.has_key(rec):
+				(name, handler) = tc6_records[rec]
+			else:
+				name = 'Record %x' % rec
+				handler = None
+			if not handler:
+				handler = 'tc6_record'
+			add_pgiter(self.page, name, 'c602', handler, self.data[off - 3:off + length], self.parent)
+			off += length
 
 class gc6_parser:
 	def __init__(self, page, data, parent):
@@ -34,7 +53,22 @@ class gc6_parser:
 	def parse(self):
 		pass
 
+def add_tc6_header(hd, size, data):
+	off = 0
+	(ident, off) = rdata(data, off, '35s')
+	add_iter(hd, 'Identifier', ident, off - 35, 35, '35s')
+
+def add_record(hd, size, data):
+	off = 0
+	(typ, off) = rdata(data, off, '<B')
+	add_iter(hd, 'Record type', typ, off - 1, 1, '<B')
+	(length, off) = rdata(data, off, '<H')
+	add_iter(hd, 'Record length', length, off - 2, 2, '<H')
+	return off
+
 c602_ids = {
+	'tc6_header': add_tc6_header,
+	'tc6_record': add_record,
 }
 
 def parse_spreadsheet(data, page, parent):
