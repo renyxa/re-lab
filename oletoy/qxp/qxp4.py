f683c416fd383950331ae3da2495ed46cb6cb33a
Alex P <alex.pantec@gmail.com>
2017-06-10 21:55:16 +0300

    qxp: master page link

--- oletoy/qxp/qxp4.py
+++ oletoy/qxp/qxp4.py
@@ -321,7 +321,10 @@ def add_page(hd, size, data, fmt, version, obfctx):
 		(id, off) = rdata(data, off, fmt('I'))
 		add_iter(hd, 'ID?', hex(id), off - 4, 4, fmt('I'), parent=block_iter)
 		hd.model.set(block_iter, 1, hex(id))
-		off += 18
+		off += 4
+		(master_ind, off) = rdata(data, off, fmt('H'))
+		add_iter(hd, 'Master page index', '' if master_ind == 0xffff else master_ind, off - 2, 2, fmt('H'), parent=block_iter)
+		off += 12
 		off = add_margins(hd, size, data, off, fmt, block_iter)
 		off = add_page_columns(hd, size, data, off, fmt, block_iter)
 		off += 4
