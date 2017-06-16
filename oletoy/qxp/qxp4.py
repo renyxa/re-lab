6e1b70d8d20a501b8a90586bcd16ddd297a59cdd
Alex P <alex.pantec@gmail.com>
2017-06-17 00:26:31 +0300

    qxp4: doc tail on error

--- oletoy/qxp/qxp4.py
+++ oletoy/qxp/qxp4.py
@@ -14,6 +14,7 @@
 # USA
 #
 
+import traceback
 from utils import *
 from qxp import *
 
@@ -150,28 +151,32 @@ def handle_doc(page, data, parent, fmt, version, obfctx, nmasters):
 	m = 0
 	while off < len(data):
 		start = off
-		(size, off) = rdata(data, off + 2, fmt('I'))
-		npages_map = {1: 'Single', 2: 'Facing'}
-		(npages, off) = rdata(data, off, fmt('H'))
-		if size & 0xffff == 0:
+		try:
+			(size, off) = rdata(data, off + 2, fmt('I'))
+			npages_map = {1: 'Single', 2: 'Facing'}
+			(npages, off) = rdata(data, off, fmt('H'))
+			if size & 0xffff == 0:
+				add_pgiter(page, 'Tail', 'qxp4', (), data[start:], parent)
+				break
+			off = start + 6 + size + 16 + npages * 12
+			(name_len, off) = rdata(data, off, fmt('I'))
+			(name, _) = rcstr(data, off)
+			off += name_len
+			pname = '[%d] %s%s page' % (i, key2txt(npages, npages_map), ' master' if m < nmasters else '')
+			if len(name) != 0:
+				pname += ' "%s"' % name
+			(objs, off) = rdata(data, off, fmt('I'))
+			pgiter = add_pgiter(page, pname, 'qxp4', ('page', fmt, version, obfctx), data[start:off], parent)
+			objs = obfctx.deobfuscate(objs & 0xffff, 2)
+			obfctx = obfctx.next()
+			for j in range(0, objs):
+				(obfctx, off) = handle_object(page, data, off, pgiter, fmt, version, obfctx, j)
+			i += 1
+			m += 1
+		except:
+			traceback.print_exc()
 			add_pgiter(page, 'Tail', 'qxp4', (), data[start:], parent)
 			break
-		off = start + 6 + size + 16 + npages * 12
-		(name_len, off) = rdata(data, off, fmt('I'))
-		(name, _) = rcstr(data, off)
-		off += name_len
-		pname = '[%d] %s%s page' % (i, key2txt(npages, npages_map), ' master' if m < nmasters else '')
-		if len(name) != 0:
-			pname += ' "%s"' % name
-		(objs, off) = rdata(data, off, fmt('I'))
-		pgiter = add_pgiter(page, pname, 'qxp4', ('page', fmt, version, obfctx), data[start:off], parent)
-		objs = obfctx.deobfuscate(objs & 0xffff, 2)
-		obfctx = obfctx.next()
-		for j in range(0, objs):
-			(obfctx, off) = handle_object(page, data, off, pgiter, fmt, version, obfctx, j)
-		i += 1
-		m += 1
-
 
 handlers1 = {
 	2: ('Print settings',),
