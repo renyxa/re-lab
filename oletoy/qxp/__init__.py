f4e4463488c999218853fceed9ab565975babb0b
David Tardon <dtardon@redhat.com>
2017-06-22 10:42:10 +0200

    qxp: fix obf. seed rotation

--- oletoy/qxp/__init__.py
+++ oletoy/qxp/__init__.py
@@ -22,6 +22,8 @@ import qxp4
 
 class ObfuscationContext:
 	def __init__(self, seed, inc):
+		assert seed & 0xffff == seed
+		assert inc & 0xffff == inc
 		self.seed = seed
 		self.inc = inc
 
@@ -32,7 +34,26 @@ class ObfuscationContext:
 		return ObfuscationContext((self.seed + 0xffff - self.inc) & 0xffff, self.inc)
 
 	def next_shift(self, shift):
-		return ObfuscationContext(((self.seed | (0xffff << 16)) >> shift) & 0xffff, self.inc)
+		# This is a modified rotation. The lower bits in the old value
+		# are moved into the higher bits in the new value, with the
+		# following modifications:
+		# 1. the higher bit of the old value is added
+		# 2. all bits higher than the lowest 1 are filled with 1, e.g.,
+		#	 0b0010 changes into 0b1110.
+		mask = 0xffff >> (16 - shift)
+		def fill(val):
+			r = shift
+			v = val
+			# find the lowest '1'
+			while v & 1 == 0 and r > 0:
+				v >>= 1
+				r -= 1
+			s = shift - r
+			m = (0xffff >> s) << s
+			return (val | m) & mask
+		highinit = self.seed & mask
+		high = fill(highinit | (self.seed >> 15)) << (16 - shift)
+		return ObfuscationContext(high | (self.seed >> shift), self.inc)
 
 	def deobfuscate(self, value, n):
 		return qxp.deobfuscate(value, self.seed, n)
