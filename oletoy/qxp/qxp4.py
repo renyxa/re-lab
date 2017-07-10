19915412d2511f04875a29f6791a5b1aa1f0b588
Aleksas Pantechovskis <alex.pantec@gmail.com>
2017-07-10 11:33:23 +0300

    qxp4: fix track amount

--- oletoy/qxp/qxp4.py
+++ oletoy/qxp/qxp4.py
@@ -1090,9 +1090,9 @@ def _add_char_format(hd, size, data, offset, fmt, version):
 	add_iter(hd, 'Scale type', key2txt(scale_type, scale_type_map), off - 2, 2, fmt('H'))
 	(shade, off) = rfract(data, off, fmt)
 	add_iter(hd, 'Shade', '%.2f%%' % (shade * 100), off - 4, 4, '4s')
-	off += 6
-	(track, off) = rdata(data, off, fmt('H'))
-	add_iter(hd, 'Track amount', track, off - 2, 2, fmt('H'))
+	off += 4
+	(track, off) = rfract(data, off, fmt)
+	add_iter(hd, 'Track amount', track, off - 4, 4, '4s')
 	off = add_dim(hd, size, data, off, fmt, 'Baseline shift')
 	# TODO: this is baseline shift, but I don't understand yet how it's saved
 	off += 2
