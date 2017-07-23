df67ca0b3178864356a3cef8bef1bd8ac6c4ccd8
David Tardon <dtardon@redhat.com>
2017-07-23 15:19:33 +0200

    qxp33: fix parsing of tabs

--- oletoy/qxp/qxp33.py
+++ oletoy/qxp/qxp33.py
@@ -675,7 +675,7 @@ def _add_para_format(hd, size, data, off, fmt, version):
 		off = add_dim(hd, size, data, off, fmt, 'From right', ruleiter)
 		(roff, off) = rfract(data, off, fmt)
 		add_iter(hd, 'Offset', '%.2f%%' % (roff * 100), off - 4, 4, fmt('i'), parent=ruleiter)
-	off += 4
+	off += 8
 	for i in range(0, 20):
 		tabiter = add_iter(hd, 'Tab %d' % (i + 1), '', off, 8, '8s')
 		off = add_tab(hd, size, data, off, fmt, version, tabiter)
