b21896fe242fb574679da4bde3656e91bfbcbacf
David Tardon <dtardon@redhat.com>
2017-07-23 10:40:20 +0200

    qxp1: parse records

--- oletoy/qxp/qxp1.py
+++ oletoy/qxp/qxp1.py
@@ -27,7 +27,47 @@ def add_header(hd, size, data, dummy, version):
     add_iter(hd, 'Version', key2txt(ver, version_map), off - 2, 2, '>H')
     return (None, size)
 
-def handle_document(page, data, parent, fmt, version, hdr):
-    return ((), ())
+def parse_record(page, data, offset, parent, version, name):
+	(length, off) = rdata(data, offset, '>I')
+	add_pgiter(page, name, 'qxp1', ('record', version), data[off - 4:off + length], parent)
+	return off + length
+
+def parse_formats(page, data, offset, parent, version, name, hdl, size):
+	(length, off) = rdata(data, offset, '>I')
+	end = off + length
+	reciter = add_pgiter(page, name, 'qxp1', ('record', version), data[offset:end], parent)
+	i = 0
+	while off < end:
+		add_pgiter(page, '[%d]' % i, 'qxp1', (hdl, version), data[off:off + size], reciter)
+		off += size
+		i += 1
+	return off
+
+def parse_pages(page, data, offset, parent, version):
+	pass
+
+def handle_document(page, data, parent, dummy, version, hdr):
+	off = 0
+	off = parse_formats(page, data, off, parent, version, 'Character formats', 'char_format', 16)
+	off = parse_formats(page, data, off, parent, version, 'Paragraph formats', 'para_format', 150)
+	off = parse_record(page, data, off, parent, version, 'Unknown') # ???
+	pagesiter = add_pgiter(page, 'Pages', 'qxp1', (), data[off:], parent)
+	return parse_pages(page, data, off, pagesiter, version)
+
+def add_record(hd, size, data, version, dummy):
+	(length, off) = rdata(data, 0, '>I')
+	add_iter(hd, 'Length', length, off - 4, 4, '>I')
+
+def add_char_format(hd, size, data, version, dummy):
+	pass
+
+def add_para_format(hd, size, data, version, dummy):
+	pass
+
+ids = {
+	'char_format': add_char_format,
+	'para_format': add_para_format,
+	'record': add_record,
+}
 
 # vim: set ft=python sts=4 sw=4 noet:
