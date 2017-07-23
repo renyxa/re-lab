b526d0b269a98df87ff5c1fb3a75cf35b7b6f075
David Tardon <dtardon@redhat.com>
2017-07-23 10:34:14 +0200

    qxp1: v.1 is big endian

--- oletoy/qxp/qxp1.py
+++ oletoy/qxp/qxp1.py
@@ -18,13 +18,13 @@ import traceback
 from utils import *
 from qxp import *
 
-def add_header(hd, size, data, fmt, version):
+def add_header(hd, size, data, dummy, version):
     off = 0
     version_map = {0x1c: '???', 0x20: '1.10'}
-    (ver, off) = rdata(data, off, fmt('H'))
-    add_iter(hd, 'Version', key2txt(ver, version_map), off - 2, 2, fmt('H'))
-    (ver, off) = rdata(data, off, fmt('H'))
-    add_iter(hd, 'Version', key2txt(ver, version_map), off - 2, 2, fmt('H'))
+    (ver, off) = rdata(data, off, '>H')
+    add_iter(hd, 'Version', key2txt(ver, version_map), off - 2, 2, '>H')
+    (ver, off) = rdata(data, off, '>H')
+    add_iter(hd, 'Version', key2txt(ver, version_map), off - 2, 2, '>H')
     return (None, size)
 
 def handle_document(page, data, parent, fmt, version, hdr):
